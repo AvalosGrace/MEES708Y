bfcb16fdd2b2cd4b99af9f4e8dcc486d7207980f
AvalosGrace <grace.avalos@umces.edu>
2022-11-01 15:38:03 -0400

    Update data_loading.R

--- code/data_loading.R
+++ code/data_loading.R
@@ -1,1 +1,26 @@
 # Data ----
+
+worm <- read.table("./data/worms.missing.txt", head = TRUE)
+head(worm)
+is.data.frame(worm)
+
+worm = worm[order(worm$Area),]
+
+tmp <- scan("./data/Lengths.dat",
+            what=list(Name="",Family="",Length=0),
+            na.string=".")
+
+marine = as.data.frame(tmp)
+marine[order(marine$Family,marine$Length),]  #base R method
+
+#data.point-----
+
+
+#tibble /dplyr------
+library(dplyr)
+worm_tbl = as_tibble(worm)
+
+marine %>%
+  #arrange(Family,Length) %>% #dplyr methods
+  group_by(Family) %>%
+  summarise(Length = mean(Length))
