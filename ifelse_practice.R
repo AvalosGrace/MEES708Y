a03f18f3e1d40e276fcb57dd721a36f30b9670d2
AvalosGrace <grace.avalos@umces.edu>
2022-11-01 16:42:57 -0400

    Create ifelse_practice.R

---
+++ ifelse_practice.R
@@ -0,0 +1,60 @@
+# if else
+temp = -50
+
+if(temp < 0) {
+  print("It's freezing!")
+  if (temp < 32){
+    print("And kids don't go to school.")
+  }
+} else {
+  print("It's not freezing.")
+}
+
+checkweather <- function(x){
+  #x is temperatue, degrees c
+  # verbose is logical value, indicating print resultz
+  if (FALSE) {
+    x = 5
+  }
+  
+  #xf <- 32 + 1.8*x
+  #print(paste0("In Farenheit it is ", xf, "."))
+  if (x < 0) {
+    if (verbose){
+      print("It's freezing!")
+    }
+      if (x < 32){
+        if (verbose) {
+          print("And kids don't go to school.")
+        }
+      }
+    } else {
+      if (verbose) {
+        print("It's not freezing.")
+      }
+    }
+  32 + 1.8*x
+  #return(xf)
+}
+
+tempF = checkweather(55, verbose = TRUE)
+checkweather(50)
+
+#leapyear function practice
+
+is.leap <- function(x) {
+  if x %% 4{
+    if x %% 100{
+      else {
+        if x %% 400{
+          print("It's a leap year.")
+        else {
+          print("It's a normal year.")
+        }
+      }
+    }
+  } else {
+    print("It's a normal year.")
+  }
+  }
+}
